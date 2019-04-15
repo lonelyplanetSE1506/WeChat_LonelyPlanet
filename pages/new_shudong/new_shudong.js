d5fff606e04433688cb658086f4091486d885bfc
Xue <1252223400@qq.com>
2019-04-15 13:58:14 +0800

    add top wish

--- pages/new_shudong/new_shudong.js
+++ pages/new_shudong/new_shudong.js
@@ -6,10 +6,11 @@ Page({
   data: {
     current: 'tab1',
     list: [],
+    hot_list: [],
     showTab1: true,
     showTab2: false
   },
-  
+  /*
   collection: function (e) {
     console.log(e)
     var that = this;
@@ -55,7 +56,7 @@ Page({
         }
       }
     })
-  },
+  },*/
 
   handleChange({ detail }) {
     this.setData({
@@ -104,43 +105,31 @@ Page({
       }
     })
     //console.log(list)  
+    wx.request({
+      url: app.globalData.myUrl + '/Top10Wish/', //请求的URL地址
+      method: "GET", //请求方式
+      data: {}, //是否有数据传到服务器
+      success: function (res) {
+        var listData = res.data;
 
+        console.log(res.data)
+        console.log(listData)
+        if (listData == null) {
+          var toastText = "返回数据失败" + res.data.errMsg;
+          wx.showToast({
+            title: toastText,
+            icon: "",
+            duration: 2000
+          });
+        } else {
+          that.setData({
+            hot_list: listData.top10WishList,
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
 
   tapOneWish:function(e){
     wx.navigateTo({
