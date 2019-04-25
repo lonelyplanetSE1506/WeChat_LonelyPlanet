ec25b69955f3728984ac7ce6723ff72b36965d47
Xue <1252223400@qq.com>
2019-04-25 16:21:14 +0800

    add shanliao chat

--- pages/shanliao/shanliao.js
+++ pages/shanliao/shanliao.js
@@ -1,74 +1,39 @@
-// pages/shanliao/shanliao.js
+
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: {},
+    openid: null
   },
 
-  //点击随机匹配按钮
-  pipei: function () {
-    console.log("匹配成功，进入一对一聊天页面")
-    wx.navigateTo({
-      url: '../1to1/1to1',
+  onLoad: function () {
+    var _this = this;
+    wx.getStorage({
+      key: 'my_openID',
+      success: function (res) {
+        _this.setData({
+          openid: res.data
+        })
+        console.log("openid: "+_this.data.openid)
+      },
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  join: function () {
+    var that = this
+    wx.request({
+      url: 'http://localhost:8080/chat/random',
+      method: 'POST',
+      success: function (e) {
+        console.log("e.data.data: " + e.data.data)
+        wx.navigateTo({
+          url: '/pages/chat/chat?to=' + e.data.data,
+        })
+      }
+    })
 
   }
+
 })
